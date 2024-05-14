ee4954fc2874cbde5562a7237ab8bc3ca29ab15a
manojgain1988 <manojgain1988@gmail.com>
2024-05-14 21:09:58 +0600

    Login & Register

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Todo(models.Model):
+    user=models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
     text =models.CharField(max_length=200)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
--- todoapp/views.py
+++ todoapp/views.py
@@ -27,3 +27,13 @@ def deleteTodo(request, id):
 def about(request):
     return render(request, 'about.html')
 
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+
+def register(request):
+    return render(request, 'register.html')
+
